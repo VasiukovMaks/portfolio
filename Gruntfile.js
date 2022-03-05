0e77f40a597f8afc1429227770acad0b38f016e6
maxvasiukov <maxvasyukov@yandex.ru>
2022-03-05 17:35:07 +0300

    Grunt concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,13 +28,23 @@ module.exports = function(grunt) {
                     'dest/style.css': 'src/less/index.less'
                 }
             },
-        }
+        },
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: ['src/js/*.js'],
+                dest: 'dest/index.js',
+            },
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-pug');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('default', ['pug', 'less', 'watch']);
+    grunt.registerTask('default', ['pug', 'less', 'concat', 'watch']);
 
 };
